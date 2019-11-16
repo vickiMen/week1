e9b36efb9281acd54b9998f6674cc7708b0f4b63
vickiMen <vicki.menashe@gmail.com>
2019-11-16 17:29:21 +0200

    boxesHover extension

--- hoverBoxesDOM/main.js
+++ hoverBoxesDOM/main.js
@@ -2,8 +2,8 @@ const div = document.getElementById('container')
 const numOfBoxes = 6
 
 function randColor(){
-    const colors = ['red', 'pink', 'yellow', 'green', 'blue', 'black', 'purple', 'brown', 'orange', 'magenta', 'aqua', 'chartreuse', 'cyan', 'blueviolet', 'darkhaki']
-    const randColor = colors[Math.floor(Math.random() * (colors.length + 1))]
+    const colors = ['red', 'pink']/*, 'yellow', 'green', 'blue', 'black', 'purple', 'brown', 'orange', 'magenta', 'aqua', 'chartreuse', 'cyan', 'blueviolet', 'darkhaki']*/
+    const randColor = colors[Math.floor(Math.random() * (colors.length))]
     return randColor
 }
 
@@ -29,7 +29,38 @@ this.onmouseover = function(event) {
         if (newColor == currentColor) {
             event.target.style.backgroundColor = randColor()
         }
+        checkBoxes()
     }
+}
+
+let sameColors = false
+let boxesArr = []
+
+function checkBoxes(){
+    for (i=0; i<numOfBoxes; i++){
+        boxesArr.push(document.getElementById(`box${i}`).id)
+    }
+    
+    for (i=0; i<numOfBoxes; i++){
+        if (document.getElementById(`${boxesArr[i]}`).style.backgroundColor == document.getElementById(`${boxesArr[i+1]}`).style.backgroundColor){
+            sameColors = true
+        }
+        else {
+            sameColors = false
+            break
+        }
+    }
+    if (sameColors) {
+        printMessage()
+    }
+} 
+
+function printMessage() {
+    const messageDiv = document.createElement('div')
+    messageDiv.setAttribute('id', 'allColors')
+    messageDiv.setAttribute('style', 'font-family: Avenir-medium; font-size: 20px; padding: 10px; margin-left: 5px; display: block')
+    document.body.appendChild(messageDiv)
+    document.getElementById('allColors').innerHTML = 'Nice Job!'
 }
 
 
